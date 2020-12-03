2339eb34c84567aa1f336bc958dff94a43dc0977
Neil Barnwell <neil.barnwell@bybox.com>
2020-12-03 18:36:54 +0000

    Refactor into separate files

--- AdventOfCode2020.Tests/Day1.cs
+++ AdventOfCode2020.Tests/Day1.cs
@@ -1,23 +1,8 @@
-using System;
-using System.Collections.Generic;
 using NUnit.Framework;
-using System.IO;
 using System.Linq;
 
 namespace AdventOfCode2020.Tests
 {
-    public class PuzzleInputLoader 
-    {
-        public IEnumerable<T> GetInput<T>(string name)
-        {
-            var filename = $"{name}.txt";
-
-            return File.ReadLines(filename)
-                       .Select(x => Convert.ChangeType(x, typeof(T)))
-                       .Cast<T>();
-        }
-    }
-
     public class Day1
     {
         [Test]
