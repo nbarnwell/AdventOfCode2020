52a3b5dc63f40ebe234ec833a38e0070090cd7d2
Neil Barnwell <neil.barnwell@bybox.com>
2020-12-04 00:03:09 +0000

    Day 2 part 2 complete

--- AdventOfCode2020.Tests/Day2.cs
+++ AdventOfCode2020.Tests/Day2.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode2020.Tests
         [Test]
         [TestCase("Day2_Example", ExpectedResult = 2)]
         [TestCase("Day2_Input", ExpectedResult = 548)]
-        public int Check_passwords(string inputFile)
+        public int Check_sled_rental_place_passwords(string inputFile)
         {
             var input = PuzzleInputLoader.GetInput<string>(inputFile);
 
@@ -33,6 +33,27 @@ namespace AdventOfCode2020.Tests
                          CheckCharacterCount = x.Password.Count(c => c == x.CheckCharacter)
                      })
                      .Count(x => x.CheckCharacterCount >= x.Min && x.CheckCharacterCount <= x.Max);
+
+            return result;
+        }
+
+        [Test]
+        [TestCase("Day2_Example", ExpectedResult = 1)]
+        [TestCase("Day2_Input", ExpectedResult = 502)]
+        public int Check_toboggan_rental_place_passwords(string inputFile)
+        {
+            var input = PuzzleInputLoader.GetInput<string>(inputFile);
+
+            var result =
+                input.Select(x => Regex.Match(x, @"^(\d+)-(\d+) (\w): (\w+)$"))
+                     .Where(x => x.Success)
+                     .Select(x => new
+                     {
+                         CheckPositions = new int[] {int.Parse(x.Groups[1].Value), int.Parse(x.Groups[2].Value)},
+                         CheckCharacter = char.Parse(x.Groups[3].Value),
+                         Password       = x.Groups[4].Value
+                     })
+                     .Count(x => x.CheckPositions.Select(cp => x.Password[cp-1]).Count(c => c == x.CheckCharacter) == 1);
 
             return result;
         }
