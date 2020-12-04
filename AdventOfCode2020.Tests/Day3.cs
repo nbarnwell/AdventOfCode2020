6f7afdbc2eec67a9b5fb5d9f4bfcec42147baabd
Neil Barnwell <neil.barnwell@bybox.com>
2020-12-04 00:25:35 +0000

    Day 3 part 1 complete

---
+++ AdventOfCode2020.Tests/Day3.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day3
+    {
+        [Test]
+        [TestCase("Day3_Example", ExpectedResult = 7)]
+        [TestCase("Day3_Input", ExpectedResult = 286)]
+        public int Part1(string inputFile)
+        {
+            var input = PuzzleInputLoader.GetInput<string>(inputFile).ToArray();
+
+            return Enumerable.Range(0, input.Count())
+                      .Select(row => new {X = (row * 3) % input[row].Length, Y = row})
+                      .Count(x => input[x.Y][x.X] == '#');
+        }
+    }
+}
