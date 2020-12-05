7658462fce2c89eb3a27779cd54104b569c1cb70
Neil Barnwell <neil.barnwell@bybox.com>
2020-12-05 00:36:43 +0000

    Day 4 Part 2 started

--- AdventOfCode2020.Tests/Day4.cs
+++ AdventOfCode2020.Tests/Day4.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 
 namespace AdventOfCode2020.Tests
@@ -8,15 +11,103 @@ namespace AdventOfCode2020.Tests
     {
         [Test]
         [TestCase("Day4_Example", ExpectedResult = 2)]
-        [TestCase("Day4_Input", ExpectedResult = 190)]
+        [TestCase("Day4_Input", ExpectedResult   = 190)]
         public int Part1(string inputFile)
         {
             var input = PuzzleInputLoader.GetInput<string>(inputFile);
 
-            var fields         = new[] {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
+            var fields         = new[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
             var requiredFields = fields.Where(x => x != "cid").ToArray();
 
             return GetPassports(input).Count(x => x.Keys.Intersect(requiredFields).Count() == requiredFields.Length);
+        }
+
+        [Test]
+        [TestCase("Day4_Example", ExpectedResult = 2)]
+        [TestCase("Day4_Example2_InvalidPassports", ExpectedResult = 0)]
+        [TestCase("Day4_Example2_ValidPassports", ExpectedResult = 4)]
+        [TestCase("Day4_Input", ExpectedResult   = 190)]
+        public int Part2(string inputFile)
+        {
+            var input = PuzzleInputLoader.GetInput<string>(inputFile);
+
+            bool IsNumberBetween(string inputValue, int lower, int higher)
+            {
+                var numeric = int.Parse(inputValue);
+
+                return numeric >= lower && numeric <= higher;
+            }
+
+            bool IsHeightBetween(string inputValue, int lower, int higher, string uom)
+            {
+                var regexResult = Regex.Match(inputValue, @"(\d+)(cm|in)");
+                var number      = regexResult.Groups[1].Value;
+                var units       = regexResult.Groups[2].Value;
+
+                return units == uom && IsNumberBetween(number, lower, higher);
+            }
+
+            bool IsHeightInCentimetresBetween(string inputValue, int lower, int higher)
+            {
+                return IsHeightBetween(inputValue, lower, higher, "cm");
+            }
+
+            bool IsHeightInInchesBetween(string inputValue, int lower, int higher)
+            {
+                return IsHeightBetween(inputValue, lower, higher, "in");
+            }
+
+            bool IsValidHairColour(string inputValue)
+            {
+                return Regex.IsMatch(inputValue, @"#[a-f0-9]{6}");
+            }
+
+            bool IsValidEyeColour(string inputValue)
+            {
+                return Regex.IsMatch(inputValue, @"amb|blu|brn|gry|grn|hzl|oth");
+            }
+
+            bool IsValidPassportId(string inputValue)
+            {
+                return Regex.IsMatch(inputValue, @"\d{9}");
+            }
+
+            var fields = new Dictionary<string, Predicate<string>>
+            {
+                { "byr", s => IsNumberBetween(s, 1920, 2002) },
+                { "iyr", s => IsNumberBetween(s, 2010, 2020) },
+                { "eyr", s => IsNumberBetween(s, 2020, 2030) },
+                { "hgt", s => IsHeightInCentimetresBetween(s, 150, 193) || IsHeightInInchesBetween(s, 59, 76) },
+                { "hcl", s => IsValidHairColour(s) },
+                { "ecl", s => IsValidEyeColour(s) },
+                { "pid", s => IsValidPassportId(s) },
+                { "cid", s => true }
+            };
+
+            var requiredFields = fields.Keys.Where(x => x != "cid").ToArray();
+
+            return GetPassports(input).Where(x => x.Keys.Intersect(requiredFields).Count() == requiredFields.Length)
+                                      .ForEach((x, idx) =>
+                                      {
+                                          Console.WriteLine($"Passport {idx}:");
+                                          var isPassportValid = true;
+                                          foreach (var kvp in x)
+                                          {
+                                              var isValid = fields[kvp.Key](kvp.Value);
+
+                                              if (!isValid)
+                                              {
+                                                  isPassportValid = false;
+                                              }
+
+                                              var isValidText = isValid ? " (Valid)" : " (Invalid)";
+                                              Console.WriteLine($" - {kvp.Key} = {kvp.Value} {isValidText}");
+                                          }
+
+                                          var isPassportValidText = isPassportValid ? " Valid" : " Invalid";
+                                          Console.WriteLine($" Passport is {isPassportValidText}");
+                                      })
+                                      .Count(x => x.All(kvp => fields[kvp.Key](kvp.Value)));
         }
 
         private IEnumerable<IDictionary<string, string>> GetPassports(IEnumerable<string> batchFile)
@@ -29,7 +120,7 @@ namespace AdventOfCode2020.Tests
                     var kvp = line.Split(' ').Select(x => x.Split(':'));
                     foreach (var item in kvp)
                     {
-                        passport.Add(item[0], item[1]);
+                        passport.Add(item[0].Trim(), item[1].Trim());
                     }
                 }
                 else
@@ -37,6 +128,11 @@ namespace AdventOfCode2020.Tests
                     yield return passport;
                     passport = new Dictionary<string, string>();
                 }
+            }
+
+            if (passport.Values.Any())
+            {
+                yield return passport;
             }
         }
     }
--- AdventOfCode2020.Tests/EnumerableExtensions.cs
+++ AdventOfCode2020.Tests/EnumerableExtensions.cs
@@ -15,5 +15,26 @@ namespace AdventOfCode2020.Tests
                 yield return item;
             }
         }
+
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            foreach (var item in items)
+            {
+                action(item);
+
+                yield return item;
+            }
+        }
+
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> items, Action<T, int> action)
+        {
+            int i = 0;
+            foreach (var item in items)
+            {
+                action(item, i++);
+
+                yield return item;
+            }
+        }
     }
 }
