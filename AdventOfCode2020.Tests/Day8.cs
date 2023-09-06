d753d6b452aa4822aa587ea8ff9ef4e86306c1f1
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2023-09-06 23:18:45 +0100

    Day 8 part 2 complete

--- AdventOfCode2020.Tests/Day8.cs
+++ AdventOfCode2020.Tests/Day8.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 
@@ -14,6 +15,73 @@ namespace AdventOfCode2020.Tests
         {
             var instructions = PuzzleInputLoader.GetInputLines<string>(inputFile).ToList();
 
+            return Compute(instructions);
+        }
+
+        [Test]
+        [TestCase("Day8_Example", ExpectedResult = 5)]
+        [TestCase("Day8_Input", ExpectedResult = 1217)]
+        public int Part2(string inputFile)
+        {
+            var instructions = PuzzleInputLoader.GetInputLines<string>(inputFile).ToList();
+
+            var computer = new Computer(instructions);
+
+            computer.Compute();
+
+            return computer.Accumulator;
+        }
+
+        private class Computer
+        {
+            private readonly List<string> _instructions = new List<string>();
+
+            public int Accumulator { get; private set; }
+
+            public Computer(IEnumerable<string> input)
+            {
+                _instructions.AddRange(input);
+            }
+
+            public void Compute()
+            {
+                var instructionPointer  = 0;
+                var instructionHitCount = new int[_instructions.Count];
+
+                while (instructionPointer < _instructions.Count)
+                {
+                    instructionHitCount[instructionPointer]++;
+
+                    if (instructionHitCount[instructionPointer] > 1)
+                    {
+                        break;
+                    }
+
+                    var split     = _instructions[instructionPointer].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    var operation = split[0];
+                    var args      = split.Skip(1).ToArray();
+
+                    switch (operation)
+                    {
+                        case "acc":
+                            Accumulator += Convert.ToInt32(args[0]);
+                            instructionPointer++;
+                            break;
+                        case "jmp":
+                            instructionPointer += Convert.ToInt32(args[0]);
+                            break;
+                        case "nop":
+                            instructionPointer++;
+                            break;
+                        default:
+                            throw new NotImplementedException(operation);
+                    }
+                }
+            }
+        }
+
+        private static int Compute(List<string> instructions)
+        {
             int instructionPointer  = 0;
             int accumulator         = 0;
             var instructionHitCount = new int[instructions.Count];
